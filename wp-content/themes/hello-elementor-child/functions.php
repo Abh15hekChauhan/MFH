af62a5d1254fcf078b95978a231a4294dbab9bef
Abhishek Chauhan <abhishekchauhan725@gmail.com>
2023-09-29 12:59:26 +0530

    first commit

---
+++ wp-content/themes/hello-elementor-child/functions.php
@@ -0,0 +1,11 @@
+<?php
+function wgl_child_scripts() {
+	wp_enqueue_style( 'wgl-parent-style', get_template_directory_uri(). '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'wgl_child_scripts' );
+
+wp_enqueue_script( 'Custom-js', get_stylesheet_directory_uri() . '/assets/js/script.js', array(), false, true );
+wp_enqueue_style( 'Custom-style', get_stylesheet_directory_uri() . '/style.css');
+
+?>
+
